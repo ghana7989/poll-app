c021752780f4646cf43a0885793b4cc16f8726fe
Pavan Chindukuri <chindukuripavan@gmail.com>
2026-01-14 10:21:26 +0530

    Enhance App component to display additional Supabase environment variables including URL, publishable key, app URL, and app name.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ function App() {
 	return (
 		<>
 			{JSON.stringify(import.meta.env, null, 2)}
-			{import.meta.env.VITE_SUPABASE_URL}
+			<br />
+			<h1> Supabase URL </h1>
+			<p>{import.meta.env.VITE_SUPABASE_URL}</p>
+			<h1> Supabase Publishable Default Key </h1>
+			<p>{import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY}</p>
+			<h1> App URL </h1>
+			<p>{import.meta.env.VITE_APP_URL}</p>
+			<h1> App Name </h1>
+			<p>{import.meta.env.VITE_APP_NAME}</p>
 		</>
 	)
 }
